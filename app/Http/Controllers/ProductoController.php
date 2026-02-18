ebb5534b39190597fc4bdc0470898cd668b7d409
Josmy <oswaldorigoberto@gmail.com>
2026-02-18 12:40:07 -0600

    Add new product image for item 1771437510

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -126,6 +126,117 @@ class ProductoController extends Controller
         }
     }
 
-    //
-}
+    public function show(string $id)
+    {
+        try{
+            $producto = Producto::with(['marca','categoria','imagenes'])->findOrFail($id);
+            return response()->json($producto);
+        }catch(ModelNotFoundException $e){
+            return response()->json([
+                'message' => 'No se ha encontrado el producto con ID = ' . $id
+            ],404);
+        }
+    }
 
+    public function update(Request $request, string $id)
+    {
+        try{
+            //obtenemos el producto de bd
+            $producto = Producto::with('imagenes')->findOrFail($id);
+            //veriflicanmos se sea un objeto el que se recibe
+            if(!$request->has('producto')){
+                return response()->json([
+                    'message' => 'El objeto producto es requerido'
+                ],422);
+            }
+            //decodificamos el objeto producto
+            $productoData = json_decode($request->producto,true);
+            //verificamos que tenga un formato válido
+            if(json_last_error() !== JSON_ERROR_NONE){
+                return response()->json([
+                    'message' => 'El JSON enviado en producto no es válido',
+                    'error' => json_last_error_msg()
+                ],422);
+            }
+            //validamos estructura 
+            $validator = Validator::make($productoData, [
+                'nombre' => 'required|string|max:80|unique:productos,nombre,' .$id,
+                'descripcion' => 'required|string|max:200',
+                'precio' => 'required|numeric|min:0',
+                'stock' => 'required|integer|min:0',
+                'modelo' => 'required|string|max:50',
+                'marca.id' => 'required|exists:marcas,id',
+                'categoria.id' => 'required|exists:categorias,id',
+                'activo' => 'required|boolean',
+            ]);
+            if($validator->fails()){
+                return response()->json([
+                    'message' => 'Existen errores de validación',
+                    'error' => $validator->errors()
+                ],500);
+            }
+            //iniciamos la transación
+            DB::beginTransaction();
+            //transformamos datos al formato de base de datos
+             $data = [
+                'nombre' => $productoData['nombre'],
+                'descripcion' => $productoData['descripcion'],
+                'precio' => $productoData['precio'],
+                'stock' => $productoData['stock'],
+                'modelo' => $productoData['modelo'],
+                'marca_id' => $productoData['marca']['id'],
+                'categoria_id' => $productoData['categoria']['id'],
+                'activo' => $productoData['activo'],
+            ];
+            //guardamos en producto
+            $producto->update($data);
+            //gestionamos las imagenes
+           if($request->hasFile('imagenes')){
+                //eliminamos fisicamente las imagenes anteriores
+                foreach($producto->imagenes as $img){
+                    $ruta = public_path('images/productos/'. $img->nombre);
+                    if(file_exists($ruta)){
+                        unlink($ruta); //borramos el archivo físico
+                    }
+                    //borramos el registro de imagenes
+                    $img->delete();
+                } 
+                //guardamos las nuevas imágenes
+                 foreach($request->file('imagenes') as $file){
+                    //cambiamos el nombre de la imagen
+                    $nombreImagen = time().'_'.$file->getClientOriginalName();
+                    $rutaDestino = public_path('images/productos');
+                    //si no existe la carpeta la creamos
+                    if(!file_exists($rutaDestino)){
+                        mkdir($rutaDestino, 0755, true);
+                    }
+                    //copiamos el archivo a la ruta destino
+                    $file->move($rutaDestino, $nombreImagen);
+                    //guardamos el nombre de la imagen en la tabla
+                    //imagenes a través del modelo Imagen
+                    Imagen::create([
+                        'nombre' => $nombreImagen,
+                        'producto_id' => $producto->id
+                    ]);
+                }     
+           }
+           //confirmamos la transacción
+           DB::commit();
+           return response()->json([
+                'message' => 'Producto actualizado correctamente',
+                'producto' => $producto->load(['marca','categoria','imagenes'])
+           ],202);
+        }catch(ModelNotFoundException $e){
+            return response()->json([
+                'message' => 'No se encuentra el producto con ID = ' . $id
+            ],404);
+        }catch(\Exception $e){
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Error al actualizar el producto',
+                'error' => $e->getMessage()
+            ],500);
+        }
+    }
+    public function destroy(string $id){}
+}
